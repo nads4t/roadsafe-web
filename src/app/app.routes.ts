7b4804bb1f845adb57d64a1fdb951438abf481a7
nads4t <leanjedfreydeduque@gmail.com>
2025-04-26 19:37:55 +0800

    added auth guard sa routes need na talaga mag login muna pero testing lang

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-
+import { authGuard } from './auth.guard'; // <-- Import the guard!
 
 export const routes: Routes = [
-  {path:'', component:HomeComponent},
-  {path:'login', component:LoginComponent}
+  { path: '', component: HomeComponent, canActivate: [authGuard] }, // <-- 🔒 Protected!
+  { path: 'login', component: LoginComponent }
 ];
